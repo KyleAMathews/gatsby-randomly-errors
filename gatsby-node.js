d97146041c4dec8702945523a14f27c1c60be28c
Kyle Mathews <mathews.kyle@gmail.com>
2022-02-22 11:51:13 -0800

    always warn

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,8 +3,8 @@ let isInitialBuild = true
  * @type {import('gatsby').GatsbyConfig['sourceNodes']}
  */
 exports.sourceNodes = ({ reporter }) => {
+  reporter.warn(`me a warning but nots an error`)
   if (!isInitialBuild && Math.random() > 0.5) {
-    reporter.warn(`me a warning but nots an error`)
     throw new Error(`Argh, it gots me again!`)
   }
 
